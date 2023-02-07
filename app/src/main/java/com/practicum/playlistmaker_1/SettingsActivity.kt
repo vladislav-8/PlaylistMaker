7c9e377dc89f4c56bc44d5e7c10fb456bd977dee
Vlad <tchervladislav@yandex.ru>
2023-02-07 21:44:47 +0700

    Fixes / add toolbar

--- app/src/main/java/com/practicum/playlistmaker_1/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_1/SettingsActivity.kt
@@ -1,18 +1,16 @@
 package com.practicum.playlistmaker_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
-        val title = findViewById<TextView>(R.id.title2)
-
-        title.setOnClickListener {
-            finish()
-        }
+        val toolbar = findViewById<Toolbar>(R.id.settings_toolbar)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
+
 }
