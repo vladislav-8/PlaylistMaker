5cd672b18eaac96eb47ab21ded09fb80ad52971d
Vlad <tchervladislav@yandex.ru>
2023-02-14 19:04:46 +0700

    sprint9 2st hw

--- app/src/main/java/com/practicum/playlistmaker_1/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_1/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker_1
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -9,8 +12,43 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val toolbar = findViewById<Toolbar>(R.id.settings_toolbar)
         toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        val shareTextView = findViewById<TextView>(R.id.shareTextView)
+        val callTextView = findViewById<TextView>(R.id.callTextView)
+        val agreementTextView = findViewById<TextView>(R.id.agreementTextView)
+
+        shareTextView.setOnClickListener {
+            val shareText = "https://practicum.yandex.ru/profile/android-developer/"
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.putExtra(Intent.EXTRA_TEXT, shareText)
+            sendIntent.type = "text/plain"
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        callTextView.setOnClickListener {
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val sendIntent = Intent(Intent.ACTION_SENDTO)
+            sendIntent.data = Uri.parse("mailto:")
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("tchervladislav@yandex.ru"))
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+
+        }
+
+        agreementTextView.setOnClickListener {
+            val sendIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
     }
 
 }
