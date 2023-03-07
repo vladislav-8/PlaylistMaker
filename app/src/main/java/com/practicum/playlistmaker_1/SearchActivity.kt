5fada2b5924400bae8bae020348e713934936497
Vlad <tchervladislav@yandex.ru>
2023-03-07 13:56:20 +0700

    sprint10 hw

--- app/src/main/java/com/practicum/playlistmaker_1/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_1/SearchActivity.kt
@@ -10,25 +10,63 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker_1.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private val trackList = listOf(
+        Track(
+            "Smells Like Teen Spirit",
+            "Nirvana",
+            "5:01",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "Michael Jackson",
+            "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "Bee Gees",
+            "4:10",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "Led Zeppelin",
+            "5:33",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "Guns N' Roses",
+            "5:03",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+    )
 
     companion object {
         const val SEARCH_QUERY = "SEARCH_QUERY"
     }
 
+    lateinit var searchBinding: ActivitySearchBinding
     private var searchInputQuery = ""
     private lateinit var inputEditText: EditText
     private lateinit var clearImageView: ImageView
     private lateinit var toolbar: Toolbar
+    private lateinit var searchRecycler: RecyclerView
 
     private val simpleTextWatcher = object : TextWatcher {
 
         override fun onTextChanged(s: CharSequence?, s1: Int, s2: Int, s3: Int) {
             if (s.isNullOrEmpty()) {
-                findViewById<ImageView>(R.id.clearImageView).visibility = GONE
+                searchBinding.clearImageView.visibility = GONE
             } else {
-                findViewById<ImageView>(R.id.clearImageView).visibility = VISIBLE
+                searchBinding.clearImageView.visibility = VISIBLE
             }
             searchInputQuery = s.toString()
         }
@@ -40,21 +78,31 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        searchBinding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(searchBinding.root)
 
-        inputEditText = findViewById(R.id.inputEditText)
-        clearImageView = findViewById(R.id.clearImageView)
-        toolbar = findViewById(R.id.settings_toolbar)
+        inputEditText = searchBinding.inputEditText
+        clearImageView = searchBinding.clearImageView
+        toolbar = searchBinding.settingsToolbar
+        searchRecycler = searchBinding.searchRecycler
         inputEditText.requestFocus()
 
-        findViewById<Toolbar>(R.id.settings_toolbar).setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
-        findViewById<ImageView>(R.id.clearImageView).setOnClickListener { clearSearchForm() }
-        findViewById<EditText>(R.id.inputEditText).addTextChangedListener(simpleTextWatcher)
+        searchBinding.settingsToolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        searchBinding.clearImageView.setOnClickListener { clearSearchForm() }
+        searchBinding.inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        searchRecycler.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        val trackAdapter = TrackAdapter(trackList)
+        searchRecycler.adapter = trackAdapter
     }
 
 
     private fun clearSearchForm() {
-        findViewById<EditText>(R.id.inputEditText).setText("")
+        searchBinding.inputEditText.setText("")
         val view = this.currentFocus
         if (view != null) {
             val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -73,5 +121,7 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.setText(searchInputQuery)
 
     }
+
+
 }
 
---
+++ app/src/main/java/com/practicum/playlistmaker_1/Track.kt
@@ -0,0 +1,6 @@
+package com.practicum.playlistmaker_1
+
+data class Track(val trackName: String,
+                 val artistName: String,
+                 val trackTime: String,
+                 val artworkUrl100: String)
