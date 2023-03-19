53b46ff333192573ca1a54f8a28bdefe87b48c85
Vlad <tchervladislav@yandex.ru>
2023-03-19 23:07:52 +0700

    sprint11 hw

--- app/src/main/java/com/practicum/playlistmaker_1/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker_1/SearchActivity.kt
@@ -6,44 +6,29 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.practicum.playlistmaker_1.databinding.ActivitySearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
-    private val trackList = listOf(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
+    private val tracksBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(tracksBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val tracksApi = retrofit.create(TracksApi::class.java)
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackAdapter(tracks)
+
 
     companion object {
         const val SEARCH_QUERY = "SEARCH_QUERY"
@@ -83,8 +68,70 @@ class SearchActivity : AppCompatActivity() {
             LinearLayoutManager.VERTICAL,
             false
         )
-        val trackAdapter = TrackAdapter(trackList)
-        searchBinding.searchRecycler.adapter = trackAdapter
+        searchBinding.searchRecycler.adapter = adapter
+
+        searchBinding.inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchTrack()
+            }
+            false
+        }
+
+        refresh()
+    }
+
+
+    private fun refresh() {
+        searchBinding.refreshButton.setOnClickListener {
+            searchTrack()
+        }
+    }
+
+
+    private fun searchTrack() {
+        tracksApi.searchTrack(searchBinding.inputEditText.text.toString())
+            .enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (searchBinding.inputEditText.text.isNotEmpty() && !response.body()?.results.isNullOrEmpty() && response.code() == 200) {
+                        tracks.clear()
+                        tracks.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                        showPlaceholder(Placeholder.SEARCH_RESULT)
+                    } else {
+                        showPlaceholder(Placeholder.NOTHING_FOUND)
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showPlaceholder(Placeholder.INTERNET_PROBLEM)
+                }
+            })
+    }
+
+
+    fun showPlaceholder(placeholder: Placeholder) {
+        when (placeholder) {
+            Placeholder.NOTHING_FOUND -> {
+                searchBinding.searchRecycler.visibility = GONE
+                searchBinding.internetProblem.visibility = GONE
+                searchBinding.nothingFound.visibility = VISIBLE
+
+            }
+            Placeholder.INTERNET_PROBLEM -> {
+                searchBinding.searchRecycler.visibility = GONE
+                searchBinding.nothingFound.visibility = GONE
+                searchBinding.internetProblem.visibility = VISIBLE
+            }
+
+            else -> {
+                searchBinding.searchRecycler.visibility = VISIBLE
+                searchBinding.nothingFound.visibility = GONE
+                searchBinding.internetProblem.visibility = GONE
+            }
+        }
     }
 
 
@@ -108,4 +155,5 @@ class SearchActivity : AppCompatActivity() {
         searchBinding.inputEditText.setText(searchInputQuery)
     }
 }
+
 
---
+++ app/src/main/java/com/practicum/playlistmaker_1/TrackResponse.kt
@@ -0,0 +1,6 @@
+package com.practicum.playlistmaker_1
+
+class TrackResponse(
+    val resultCount: Int,
+    val results: ArrayList<Track>
+)
